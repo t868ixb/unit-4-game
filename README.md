a70a29ea7ec0eac1847855ac23ce7ade21fd42f4
“IBayon“ <IBayon@Enova.com>
2018-09-26 16:33:36 -0500

    final changes..

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # unit-4-game
+
+Welcome to the word guessig game.  I hope you enjoy it.
+
+The html file is using bootstrap.  However, it does not contain much of the power available with bootstrap.  I used it mostly for helping to layout the rows and columns.  
+
+The javascript file contains the bulk of the power behind the game.  To be honest, I find it hard to remember what's javascript vs jQuery.  I know for sure I have some jQuery in there since it's a bit easier than figuring things out with pure javascript.
+
+As for the CSS file, I admit that I'm repeating a lot fo code.  With more experience I want to focus on reducing that.
+
+ib
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,9 @@
-//<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-//$(document).ready(function () {
+// IBayon Homework 4 jQuery.  Due Wednesday September 25, 2018
 
 var randomNumber = Math.floor(Math.random() * (101 + 1) + 19);
-console.log("randonNumber var is: " + randomNumber);
+// console.log("randonNumber var is: " + randomNumber);
 var chosenOne = randomNumber;
-console.log("chosenOne var is: " + chosenOne);
+// console.log("chosenOne var is: " + chosenOne);
 $("#computer-pick").text(chosenOne);
 
 
@@ -18,14 +17,11 @@ console.log("green computer pick: " + green);
 var purple = Math.floor((Math.random() * 12) + 1);
 console.log("purple computer pick: " + purple);
 
-//put the four numbers in an array --not convinced i need this...
-
-//display the 4 random numbers not needed, just console log them to see them - done
-//each of the 4 crystals should represent one of the 4 random numbers
-// assign random number to buttons (displayed)
 var totalCount = 0;
 var wins = 0;
 var losses = 0;
+
+// assign random number to buttons (displayed)
 //button 1 - blue gem
 $('#button-1').on('click', function () {
     totalCount = totalCount + blue;
@@ -37,12 +33,14 @@ $('#button-1').on('click', function () {
         wins++;
         $('#wins').text(wins);
         console.log("Your total wins: " + wins);
+        alert("You won!!  Start a new game!");
         resetGame();
     }
     else if (chosenOne < totalCount) {
         losses++;
         $('#losses').text(losses);
         console.log("Computer total wins: " + losses);
+        alert("Your computer won!!  Start a new game!");
         resetGame();
     };
 });
@@ -57,12 +55,14 @@ $('#button-2').on('click', function () {
         wins++;
         $('#wins').text(wins);
         console.log("Your total wins: " + wins);
+        alert("You won!!  Start a new game!");
         resetGame();
     }
     else if (chosenOne < totalCount) {
         losses++;
         $('#losses').text(losses);
         console.log("Computer total wins: " + losses);
+        alert("Your computer won!!  Start a new game!");
         resetGame();
     };
 });
@@ -77,12 +77,14 @@ $('#button-3').on('click', function () {
         wins++;
         $('#wins').text(wins);
         console.log("Your total wins: " + wins);
+        alert("You won!!  Start a new game!");
         resetGame();
     }
     else if (chosenOne < totalCount) {
         losses++;
         $('#losses').text(losses);
         console.log("Computer total wins: " + losses);
+        alert("Your computer won!!  Start a new game!");
         resetGame();
     };
 });
@@ -98,17 +100,19 @@ $('#button-4').on('click', function () {
         wins++;
         $('#wins').text('wins');
         console.log("Your total wins: " + wins);
+        alert("You won!!  Start a new game!");
         resetGame();
     }
     else if (chosenOne < totalCount) {
         losses++;
         $('#losses').text(losses);
         console.log("Computer total wins: " + losses);
-        alert ()
+        alert("Your computer won!!  Start a new game!");
         resetGame();
     };
 });
 
+//this function resets the game after a win by the person or computer
 function resetGame() {
     randomNumber = Math.floor(Math.random() * (101 + 1) + 19);
     chosenOne = randomNumber;
